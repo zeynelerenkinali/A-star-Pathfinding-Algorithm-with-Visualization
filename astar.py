f38e365284b311c8ddba4db359324fa455edfc46
zeynelerenkinali <zeynel.eren.kinali55@erzurum.edu.tr>
2026-01-02 15:46:23 +0300

    .

--- astar.py
+++ astar.py
@@ -73,12 +73,23 @@ class Spot:
 	def __lt__(self, other):
 		return False
 
-
-
 def h(p1, p2):
 	# Expecting p1 and p2 as (row, col) or (x, y)
 	# Manhattan distance algorithm used to calculate distance between two points
-
 	x1, y1 = p1
 	x2, y2 = p2
+	return abs(x1 - x2) + abs(y1 - y2)
+
+def make_grid(rows, width):
+	grid = []
+	gap = width // rows
+	for i in range(rows):
+		grid.append([])
+		for j in range(rows):
+			spot = new Spot(i, j, gap, rows)
+			grid[i].append(spot)
+
+	return grid
+
+def draw_grid(win, rows, width):
 	
